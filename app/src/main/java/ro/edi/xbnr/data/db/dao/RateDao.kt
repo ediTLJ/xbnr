c00e080375010f0f76bb9ed551d76e8d5e086ced
Eduard Scarlat <edi.tlj@gmail.com>
2019-02-15 01:34:52 +0200

    fixed/optimized db queries

--- app/src/main/java/ro/edi/xbnr/data/db/dao/RateDao.kt
+++ app/src/main/java/ro/edi/xbnr/data/db/dao/RateDao.kt
@@ -10,15 +10,8 @@ import ro.edi.xbnr.model.RateMinimal
 
 @Dao
 abstract class RateDao : BaseDao<DbRate> {
-//    SELECT d1.*
-//    from docs d1
-//    left outer join docs d2
-//    on (d1.id = d2.id and d1.rev < d2.rev)
-//    where d2.id is null
-//    order by id;
-
     @Transaction
-    @Query("SELECT currency_id, code, multiplier, is_favorite, date, rate FROM rates LEFT OUTER JOIN currencies ON (rates.currency_id = currencies.id) ORDER BY date DESC LIMIT (SELECT count(*) FROM currencies)")
+    @Query("SELECT currency_id, code, multiplier, is_favorite, date, rate FROM rates LEFT OUTER JOIN currencies ON (rates.currency_id = currencies.id) WHERE date = (SELECT MAX(date) FROM rates) ORDER BY code ASC")
     protected abstract fun query(): LiveData<List<Currency>>
 
     @Transaction
@@ -26,7 +19,7 @@ abstract class RateDao : BaseDao<DbRate> {
     protected abstract fun query(id: Int, count: Int): LiveData<List<RateMinimal>>
 
     @Transaction
-    @Query("SELECT id, date, rate FROM rates WHERE currency_id = :id ORDER BY date DESC LIMIT 1")
+    @Query("SELECT id, date, rate FROM rates WHERE currency_id = :id AND date = (SELECT MAX(date) FROM rates)")
     protected abstract fun query(id: Int): LiveData<RateMinimal>
 
     /**
