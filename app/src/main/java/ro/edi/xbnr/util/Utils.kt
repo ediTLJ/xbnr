6ea37119e645ea4c9f013fddcaadd152adae1b60
Eduard Scarlat <edi.tlj@gmail.com>
2019-02-19 21:53:03 +0200

    switched to vector drawables for flags (and use Glide to load them)

--- app/src/main/java/ro/edi/xbnr/util/Utils.kt
+++ app/src/main/java/ro/edi/xbnr/util/Utils.kt
@@ -2,6 +2,23 @@ package ro.edi.xbnr.util
 
 import android.content.Context
 import android.util.TypedValue
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+
+@BindingAdapter(value = ["src", "errorSrc", "placeholderSrc"], requireAll = false)
+fun ImageView.setImageUrl(src: Int? = null, errorSrc: Int? = null, placeholderSrc: Int? = null) {
+    val builder = Glide.with(context).load(src)
+
+    errorSrc?.let {
+        builder.error(errorSrc)
+    }
+    placeholderSrc?.let {
+        builder.placeholder(placeholderSrc)
+    }
+
+    builder.fitCenter().into(this)
+}
 
 fun getColorRes(context: Context, attrRes: Int): Int {
     val outValue = TypedValue()
