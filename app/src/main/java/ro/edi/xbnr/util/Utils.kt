08ee3571e5743acd9251452d2975622341f1d610
Eduard Scarlat <edi.tlj@gmail.com>
2019-02-25 19:51:29 +0200

    fixed flag sizes & drawable xml format

--- app/src/main/java/ro/edi/xbnr/util/Utils.kt
+++ app/src/main/java/ro/edi/xbnr/util/Utils.kt
@@ -5,19 +5,24 @@ import android.util.TypedValue
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 
 @BindingAdapter(value = ["src", "errorSrc", "placeholderSrc"], requireAll = false)
 fun ImageView.setImageUrl(src: Int? = null, errorSrc: Int? = null, placeholderSrc: Int? = null) {
     val builder = Glide.with(context).load(src)
 
-    errorSrc?.let {
-        builder.error(errorSrc)
-    }
-    placeholderSrc?.let {
-        builder.placeholder(placeholderSrc)
+    with(builder) {
+        errorSrc?.let {
+            this.error(errorSrc)
+        }
+        placeholderSrc?.let {
+            this.placeholder(placeholderSrc)
+        }
+        this.transition(DrawableTransitionOptions.withCrossFade(300))
+        this.fitCenter()
     }
 
-    builder.fitCenter().into(this)
+    builder.into(this)
 }
 
 fun getColorRes(context: Context, attrRes: Int): Int {
