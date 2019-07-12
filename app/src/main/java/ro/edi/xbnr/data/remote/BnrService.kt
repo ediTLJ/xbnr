f9808d0904ecb3c01e23b8a553a430725b021be9
Eduard Scarlat <edi.tlj@gmail.com>
2019-07-12 16:49:40 +0300

    updated libs

--- app/src/main/java/ro/edi/xbnr/data/remote/BnrService.kt
+++ app/src/main/java/ro/edi/xbnr/data/remote/BnrService.kt
@@ -25,6 +25,7 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 import ro.edi.xbnr.BuildConfig
 import ro.edi.xbnr.data.remote.model.BnrDays
+import javax.net.ssl.HostnameVerifier
 
 interface BnrService {
     companion object {
@@ -42,7 +43,7 @@ interface BnrService {
                 okBuilder.addInterceptor(logging)
             }
 
-            okBuilder.hostnameVerifier { hostname, _ -> hostname == "www.bnr.ro" }
+            okBuilder.hostnameVerifier(HostnameVerifier { hostname, _ -> hostname == "www.bnr.ro" })
 
             val tikXml = TikXml.Builder()
                 .exceptionOnUnreadXml(false)
