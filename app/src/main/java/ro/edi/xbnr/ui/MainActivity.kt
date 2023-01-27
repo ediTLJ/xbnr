b09c1edc5317cfd1fec6f5aef77c506f35cecad1
Edi S <ediTLJ@users.noreply.github.com>
2023-01-27 23:03:18 +0200

    mimic previous back press behavior on Android 12+

--- app/src/main/java/ro/edi/xbnr/ui/MainActivity.kt
+++ app/src/main/java/ro/edi/xbnr/ui/MainActivity.kt
@@ -17,17 +17,24 @@ package ro.edi.xbnr.ui
 
 import android.content.Intent
 import android.content.res.Configuration
+import android.os.Build
 import android.os.Bundle
-import android.view.*
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.WindowManager
+import androidx.activity.addCallback
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.appbar.MaterialToolbar
 import ro.edi.util.applyWindowInsetsMargins
 import ro.edi.xbnr.R
 
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // logi("onCreate: $savedInstanceState")
 
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
@@ -48,6 +55,14 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, RatesFragment.newInstance())
                 .commitNow()
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                // Android 12 changed back press behavior
+                // let's call finish() when back pressed, for now... to mimic previous behavior
+                onBackPressedDispatcher.addCallback(this, true) {
+                    finish()
+                }
+            }
         }
     }
 
